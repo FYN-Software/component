ab1f8e6bca7963ad10fec60295b52af4e8d88189
Chris Kruining <chris@kruining.eu>
2018-12-27 16:16:53 +0100

    added an export file

---
+++ fyn.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import Loop from './loop.js';
+import Component from './component.js';
+import Collection from './collection.js';
+import * as Extends from './extends.js';
+
+import Event from './utilities/event.js';
+import Gunslinger from './utilities/gunslinger.js';
+import * as Animation from './utilities/animation.js';
+
+import * as Glp from '/glp/index.js';
+
+const Utilities = {
+    Code: Glp,
+};
+
+export {
+    Component,
+    Loop,
+    Collection,
+    Gunslinger,
+    Extends,
+    Event,
+    Utilities,
+}
