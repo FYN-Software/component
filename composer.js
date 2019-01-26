fae4526b6050e97acb283a40eb993ff4b997c5da
Chris Kruining <c.kruining@fyn.nl>
2019-01-26 12:57:36 +0100

    trying new method of 'autoloading'

--- composer.js
+++ composer.js
@@ -1,8 +1,18 @@
+const registration = new Map();
+
 export default class Composer
 {
     static resolve(name, type = 'js')
     {
         const [ ns, ...el ] = name.split('-');
+
+        if(registration.has(ns) === false)
+        {
+            throw new Error('Trying to resolve unknown namespace');
+        }
+
+        return registration.get(ns).apply(null, [ el, type, ns ]);
+
         let prefix;
 
         switch(ns)
@@ -24,5 +34,13 @@ export default class Composer
         }
 
         return `${prefix}/${el.join('/')}.${type}`;
+    }
+
+    static register(config)
+    {
+        for(const [ ns, cb] of Object.entries(config))
+        {
+            registration.set(ns, cb);
+        }
     }
 }
