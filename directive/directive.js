cfed4bb5d616111630cd48beda654fa943eaa2f4
Chris Kruining <c.kruining@fyn.nl>
2019-10-28 10:10:34 +0100

    converted code to proper private variables

--- directive/directive.js
+++ directive/directive.js
@@ -1,35 +1,60 @@
-const _node = Symbol('node');
-const _binding = Symbol('binding');
-
 export default class Directive
 {
+    static #registry = new Map();
+
+    #scope = null;
+    #node = null;
+    #binding = null;
+
     static get attribute()
     {
         return this.prototype.constructor.name.toLowerCase();
     }
 
-    constructor(node, binding)
+    constructor(scope, node, binding)
     {
         if(new.target.prototype.hasOwnProperty('render') === false || typeof new.target.prototype.render !== 'function')
         {
             throw new Error(`${new.target.prototype.constructor.name} does not implement method render or is not callable`);
         }
 
-        Object.defineProperty(node, '__directive__', {
-            get: () => this
-        });
-
-        this[_node] = node;
-        this[_binding] = binding;
+        this.#scope = scope;
+        this.#node = node;
+        this.#binding = binding;
     }
+
+    get scope()
+    {
+        return this.#scope;
+    }
+
 
     get node()
     {
-        return this[_node];
+        return this.#node;
     }
 
     get binding()
     {
-        return this[_binding];
+        return this.#binding;
+    }
+
+    static register(name, path)
+    {
+        this.#registry.set(name, path);
+    }
+
+    static async get(name)
+    {
+        if(this.#registry.has(name) === false)
+        {
+            throw new Error(`Directive with name '${name}' is not registered, you can do so by calling "Directive.register('name-of-directive', 'path/to/directive')"`);
+        }
+
+        return (await import(this.#registry.get(name))).default;
     }
 }
+
+Directive.register('if', './if.js');
+Directive.register('for', './for.js');
+Directive.register('switch', './switch.js');
