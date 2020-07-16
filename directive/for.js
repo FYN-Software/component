7ac84528012dec0117954abe8cb284bea11c6e24
Chris Kruining <c.kruining@fyn.nl>
2020-07-16 16:23:38 +0200

    fixed issue with  multiple runs interfering

--- directive/for.js
+++ directive/for.js
@@ -1,3 +1,4 @@
+import lock from '../../core/lock.js';
 import Component from '../component.js';
 import Directive from './directive.js';
 import { AsyncFunction } from '../binding.js';
@@ -103,59 +104,62 @@ export default class For extends Directive
 
         await this.#initialized;
 
-        const nodesToAppend = new DocumentFragment();
-        const v = await this.binding.value;
-        const d = Object.entries(Object.entries(v && v.hasOwnProperty(Symbol.iterator) ? Array.from(v) : (v || {})))
-            .map(([ c, i ]) => [ Number(c), i ]);
+        await lock(this, async () => {
+            const nodesToAppend = new DocumentFragment();
+            const v = await this.binding.value;
+            const d = Object.entries(Object.entries(v && v.hasOwnProperty(Symbol.iterator) ? Array.from(v) : (v || {})))
+                .map(([ c, i ]) => [ Number(c), i ]);
 
-        const limit = d.length; //hackedLimit;
+            const limit = d.length; //hackedLimit;
 
-        for (const [ c, [ k, it ] ] of d)
-        {
-            // TODO(Chris Kruining) Implement actual virtual scrolling...
-            if(c >= limit)
+            for (const [ c, [ k, it ] ] of d)
             {
-                break;
+                // TODO(Chris Kruining) Implement actual virtual scrolling...
+                if(c >= limit)
+                {
+                    break;
+                }
+
+                const scope = { properties: { [this.#key]: Number.tryParseInt(k), [this.#name]: it } };
+
+                if(c < limit && this.#items.length <= c)
+                {
+                    const { html: node, bindings } = await Base.parseHtml(this.owner, scope, this.#template.cloneNode(true), [ this.#key, this.#name ]);
+
+                    this.#items.push({ nodes: Array.from(node.childNodes), bindings });
+
+                    Array.from(node.children).forEach(c => c.setAttribute('invisible', ''));
+
+                    nodesToAppend.appendChild(node);
+                }
+
+                const { nodes, bindings } = this.#items[c];
+
+                for(const node of nodes.filter(n => n.nodeType === 1))
+                {
+                    node.removeAttribute('invisible');
+                    node.setAttribute('index', c);
+                }
+
+                await Promise.all(
+                    Array
+                        .from(nodesToAppend.querySelectorAll(':defined'))
+                        .filter(el => el instanceof Component)
+                        .map(el => el.isReady)
+                );
+
+                await Promise.all(bindings.map(b => b.resolve(scope, this.owner)));
+                await Promise.all(bindings.map(b => b.nodes).reduce((t, n) => [ ...t, ...n ], []).unique().map(n => Base.render(n)));
             }
 
-            const scope = { properties: { [this.#key]: Number.tryParseInt(k), [this.#name]: it } };
-
-            if(c < limit && this.#items.length <= c)
+            for(const i of range(Math.min(10, d.length), this.#items.length))
             {
-                const { html: node, bindings } = await Base.parseHtml(this.owner, scope, this.#template.cloneNode(true), [ this.#key, this.#name ]);
-
-                this.#items.push({ nodes: Array.from(node.childNodes), bindings });
-
-                Array.from(node.children).forEach(c => c.setAttribute('invisible', ''));
-
-                nodesToAppend.appendChild(node);
+                Array.from(this.#items[i].nodes).filter(n => n.nodeType === 1).forEach(c => c.setAttribute('hidden', ''));
             }
 
-            const { nodes, bindings } = this.#items[c];
+            this.node.appendChild(nodesToAppend);
+        });
 
-            for(const node of nodes.filter(n => n.nodeType === 1))
-            {
-                node.removeAttribute('invisible');
-                node.setAttribute('index', c);
-            }
-
-            await Promise.all(
-                Array
-                    .from(nodesToAppend.querySelectorAll(':defined'))
-                    .filter(el => el instanceof Component)
-                    .map(el => el.isReady)
-            );
-
-            await Promise.all(bindings.map(b => b.resolve(scope, this.owner)));
-            await Promise.all(bindings.map(b => b.nodes).reduce((t, n) => [ ...t, ...n ], []).unique().map(n => Base.render(n)));
-        }
-
-        for(const i of range(Math.min(10, d.length), this.#items.length))
-        {
-            Array.from(this.#items[i].nodes).filter(n => n.nodeType === 1).forEach(c => c.setAttribute('hidden', ''));
-        }
-
-        this.node.appendChild(nodesToAppend);
         this.node.removeAttribute('hidden');
     }
 
